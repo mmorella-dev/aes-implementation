519050c009912642a4ab8667174a6ddd312979d3
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-11-02 21:56:09 -0400

    Implement key_expansion, add tests

--- src/aes/key_expansion.cpp
+++ src/aes/key_expansion.cpp
@@ -1,28 +1,43 @@
 #include "aes.h"
+#include "init_sbox.h"
 
+#include <algorithm>
 #include <array>
+#include <bit>
+#include <numeric>
+#include <cstdint>
 
 namespace aes
 {
-    using Word = std::array<uint8_t, 4>;
+    using Bytes4 = std::array<uint8_t, 4>;
+
+    Bytes16 rc = {0xFF, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36};
 
     Bytes176 key_expansion(const Bytes16 &key)
     {
-        std::array<Word, 44> output;
-        // for (int i{}; i < 4; i++)
-        // {
-        //     // get row i
-        //     output[i] =
-        //         {key[4 * i], key[4 * i + 1], key[4 * i + 2], key[4 * i + 3]};
-        // }
-        // Word &temp = output[3];
-        // for (int i = 4; i < 44; i++)
-        // {
-        //     temp = output[i - 1];
-        //     if (i % 4 == 0) {
-        //         temp = SubWord ( RotWord (temp)) ^ Rcon[i/4];
-        //         w[i] = w[i-4] ^ temp;
-        //     }
-        // }
+        std::array<Bytes4, 44> w;
+        for (int i = 0; i < 4; i++)
+        {
+            // get row i
+            w[i] = {key[4 * i], key[4 * i + 1], key[4 * i + 2], key[4 * i + 3]};
+        }
+        Bytes4 temp;
+        for (int i = 4; i < 44; i++)
+        {
+            temp = w[i - 1];
+            if (i % 4 == 0)
+            {
+                // rotate left by 1
+                std::rotate(temp.begin(), temp.begin() + 1, temp.end());
+                // sub word
+                std::transform(temp.cbegin(), temp.cend(), temp.begin(), [](uint8_t b)
+                               { return SBOX[b]; });
+                // sub rcon
+                temp[0] ^= rc[i / 4];
+            }
+            // w[i] = w[i-1];
+            std::transform(w[i-4].cbegin(), w[i - 4].cend(), temp.cbegin(), w[i].begin(), std::bit_xor{});
+        }
+        return std::bit_cast<Bytes176>(w);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,20 @@
 #include <cassert>
 #include <iostream>
+#include <iomanip>      // std::setw
 
 #include "aes/aes.h"
 
 using namespace aes;
 
-bool bytes16_eq(const Bytes16 &first, const Bytes16 &second)
-{
+std::ostream& operator<<(std::ostream &os, const Bytes16 &first) {
     for (int i = 0; i < 16; i++)
-    {
-        if (first[i] != second[i])
-            return false;
-    }
-    return true;
-}
-void bytes16_print(const Bytes16 &first)
-{
-    for (int i = 0; i < 16; i++)
-        std::cout << "0x" << std::hex << (int)first[i] << " ";
-    std::cout << '\n';
+        os << std::hex << std::setfill('0') << std::setw(2) << (int)first[i] << " ";
+    return os;
 }
 
 bool test_sub_bytes()
 {
+    std::cout << "Testing sub_bytes... ";
     // example from page 176;
     const Bytes16 init = {
         0xEA, 0x04, 0x65, 0x85, //
@@ -36,15 +28,17 @@ bool test_sub_bytes()
         0x8C, 0xD8, 0x95, 0xA6};
     // Call sub_bytes
     Bytes16 result = sub_bytes(init, false);
-    assert(bytes16_eq(expected, result));
+    assert(expected == result);
     // Call sub_bytes inverse
     Bytes16 result2 = sub_bytes(expected, true);
-    assert(bytes16_eq(result2, init));
+    assert(result2 == init);
+    std::cout << "✅ passed.\n";
     return true;
 }
 
 bool test_shift_rows()
 {
+    std::cout << "Testing shift_rows... ";
     // example from page 179
     const Bytes16 init = {
         0x87, 0xF2, 0x4D, 0x97, //
@@ -59,12 +53,14 @@ bool test_shift_rows()
         0x46, 0xE7, 0x4A, 0xC3, //
         0xA6, 0x8C, 0xD8, 0x95  //
     };
-    assert(bytes16_eq(result, expected));
+    assert(result == expected);
+    std::cout << "✅ passed.\n";
     return true;
 }
 
 bool test_mix_columns()
 {
+    std::cout << "Testing mix_columns... ";
     // example from p. 181
     const Bytes16 init = {
         0x87, 0xF2, 0x4D, 0x97, //
@@ -79,9 +75,32 @@ bool test_mix_columns()
         0x94, 0xE4, 0x3A, 0x42, //
         0xED, 0xA5, 0xA6, 0xBC  //
     };
-    assert(bytes16_eq(result, expected));
+    
+    assert(result == expected);
     Bytes16 result2 = mix_columns(expected, true);
-    assert(bytes16_eq(result2, init));
+    assert(result2 == init);
+    std::cout << "✅ passed.\n";
+    return true;
+}
+
+bool test_key_expansion()
+{
+    std::cout << "Testing key_expansion...";
+    Bytes16 key = {0x0F, 0x15, 0x71, 0xC9, 0x47, 0xD9, 0xE8, 0x59, 0x0C, 0xB7, 0xAD, 0xD6, 0xAF, 0x7F, 0x67, 0x98};
+    std::cerr << "Key: ";
+    std::cerr << key << '\n';
+
+    Bytes176 result = key_expansion(key);
+    for (int i = 1; i < 11; i++) {
+        std::cerr << "Round " << i << ": ";
+        std::cerr << result[i] << '\n';
+    }
+    // sample data from p. 189
+    Bytes16 round1 = { 0xDC, 0x90, 0x37, 0xB0, 0x9B, 0x49, 0xDF, 0xE9, 0x97, 0xFE, 0x72, 0x3F, 0x38, 0x81, 0x15, 0xA7};
+    Bytes16 round10 = { 0xB4, 0x8E, 0xF3, 0x52, 0xBA, 0x98, 0x13, 0x4E, 0x7F, 0x4D, 0x59, 0x20, 0x86, 0x26, 0x18, 0x76 };
+    assert(result[1] == round1);
+    assert(result[10] == round10);
+    std::cout << "✅ passed.\n";
     return true;
 }
 
@@ -90,5 +109,6 @@ int main()
     assert(test_sub_bytes());
     assert(test_shift_rows());
     assert(test_mix_columns());
-    std::cout << "All passed!\n";
+    assert(test_key_expansion());
+    std::cout << "🚀 All tests passed!\n";
 }
