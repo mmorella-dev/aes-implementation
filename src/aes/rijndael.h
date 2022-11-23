765daea1539d4897ca4e02270e46c46bd5f550c4
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-11-22 20:45:41 -0500

    Replace rotl8 function with std::rotl

--- src/aes/rijndael.h
+++ src/aes/rijndael.h
@@ -3,20 +3,13 @@
 
 #include <array>
 #include <cstdint>
+#include <bit> // for std::rotl
 
 #include "galois.h"
 
 namespace rjindael {
 
 using Bytes256 = std::array<uint8_t, 256>;
-
-/// @brief Performs a left rotation on an 8-bit value
-/// @param b the byte to use
-/// @param shift The number (0–8) of bits to rotate
-/// @return The byte b rotated by left by shift
-consteval inline uint8_t rotl8(uint8_t b, uint8_t shift) {
-  return (b << shift) | (b >> (8 - shift));
-}
 
 /// @brief Generates the rjindael S-box.
 /// @return {sbox, sbox_inverse}
@@ -29,7 +22,7 @@ consteval std::pair<Bytes256, Bytes256> generate_sbox() {
     q = galois::gn_mult(q, 0xf6);  // divide q by 3
     // Affine transformation
     uint8_t affine =
-        q ^ rotl8(q, 1) ^ rotl8(q, 2) ^ rotl8(q, 3) ^ rotl8(q, 4) ^ 0x63;
+        q ^ std::rotl(q, 1) ^ std::rotl(q, 2) ^ std::rotl(q, 3) ^ std::rotl(q, 4) ^ 0x63;
     sbox[p] = affine;
     sbox_inverse[affine] = p;
   } while (p != 1);
