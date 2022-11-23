2135f1733812e8cff2e756bc5ab1be8a234b7cc8
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-11-22 19:21:14 -0500

    Refactor sub_bytes

--- src/aes/sub_bytes.cpp
+++ src/aes/sub_bytes.cpp
@@ -1,19 +1,14 @@
+#include <algorithm>
+
 #include "aes.h"
 #include "rijndael.h"
 
-#include <algorithm>
-
-namespace aes
-{
-    Bytes16 sub_bytes(const Bytes16 &input, bool inverse)
-    {
-        Bytes16 result;
-        std::transform(input.begin(), input.end(), result.begin(), [=](uint8_t b)
-                       {
-        if (!inverse)
-            return rjindael::SBOX[b];
-        else
-            return SBOX_INVERSE[b]; });
-        return result;
-    }
+namespace aes {
+Bytes16 sub_bytes(const Bytes16& input, bool inverse) {
+  Bytes16 result;
+  const auto& sbox = (!inverse) ? rjindael::SBOX : rjindael::SBOX_INVERSE;
+  std::transform(input.begin(), input.end(), result.begin(),
+                 [=](uint8_t b) { return sbox[b]; });
+  return result;
 }
+}  // namespace aes
