63f0d749dfb5a59ba1dc09e3c40eabf07178f70e
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-11-29 01:13:45 -0500

    Rename common byte16 param to "key"

--- src/aes/aes.cpp
+++ src/aes/aes.cpp
@@ -14,37 +14,37 @@
 
 namespace ranges = std::ranges;
 
-auto aes::SubBytes(std::span<byte, 16> input, bool inverse) -> void {
-  for (uint8_t &b : input) {
+auto aes::SubBytes(std::span<byte, 16> key, bool inverse) -> void {
+  for (uint8_t &b : key) {
     b = rijndael::SubByte(b, inverse);
   }
 }
 
-auto aes::ShiftRows(std::span<byte, 16> input, bool inverse) -> void {
+auto aes::ShiftRows(std::span<byte, 16> key, bool inverse) -> void {
   for (int i = 0; i < 4; i++) {
-    auto row = input.begin() + 4 * i;
+    auto row = key.begin() + 4 * i;
     auto middle = row + (!inverse ? i : 4 - i);
     std::rotate(row, middle, row + 4);
   }
 }
 
-auto aes::MixColumns(std::span<byte, 16> s, bool inverse) -> void {
+auto aes::MixColumns(std::span<byte, 16> key, bool inverse) -> void {
   // import lookup tables
   using namespace galois;
   for (int j = 0; j < 4; j++) {
     // get column vector
-    const std::array<byte, 4> a = {s[j], s[4 + j], s[8 + j], s[12 + j]};
+    const std::array<byte, 4> a = {key[j], key[4 + j], key[8 + j], key[12 + j]};
     if (!inverse) {
       uint8_t temp = a[0] ^ a[1] ^ a[2] ^ a[3];
-      s[j] = a[0] ^ temp ^ x2[a[0] ^ a[1]];
-      s[j + 4] = a[1] ^ temp ^ x2[a[1] ^ a[2]];
-      s[j + 8] = a[2] ^ temp ^ x2[a[2] ^ a[3]];
-      s[j + 12] = a[3] ^ temp ^ x2[a[3] ^ a[0]];
+      key[j] = a[0] ^ temp ^ x2[a[0] ^ a[1]];
+      key[j + 4] = a[1] ^ temp ^ x2[a[1] ^ a[2]];
+      key[j + 8] = a[2] ^ temp ^ x2[a[2] ^ a[3]];
+      key[j + 12] = a[3] ^ temp ^ x2[a[3] ^ a[0]];
     } else {
-      s[j] = xE[a[0]] ^ x9[a[3]] ^ xD[a[2]] ^ xB[a[1]];
-      s[j + 4] = xE[a[1]] ^ x9[a[0]] ^ xD[a[3]] ^ xB[a[2]];
-      s[j + 8] = xE[a[2]] ^ x9[a[1]] ^ xD[a[0]] ^ xB[a[3]];
-      s[j + 12] = xE[a[3]] ^ x9[a[2]] ^ xD[a[1]] ^ xB[a[0]];
+      key[j] = xE[a[0]] ^ x9[a[3]] ^ xD[a[2]] ^ xB[a[1]];
+      key[j + 4] = xE[a[1]] ^ x9[a[0]] ^ xD[a[3]] ^ xB[a[2]];
+      key[j + 8] = xE[a[2]] ^ x9[a[1]] ^ xD[a[0]] ^ xB[a[3]];
+      key[j + 12] = xE[a[3]] ^ x9[a[2]] ^ xD[a[1]] ^ xB[a[0]];
     }
   }
 }
@@ -54,14 +54,15 @@ const std::array<aes::byte, 16> RconValues = {
 
 auto aes::KeyExpansion(const std::span<byte, 16> key)
     -> std::array<std::array<byte, 16>, 11> {
-  std::array<std::array<byte, 4>, 44> w;
+  std::array<std::array<byte, 4>, 44> words;
+  // Case 1: words[i] = k[i];
+  // Case 2: words[i] = words[i-4] ^ SubWord(RotWord(words[i-1])) ^ Rcon[i/4]
+  // Case 3: words[i] = words[i-4] ^ words[i-1]
   for (int i = 0; i < 44; i++) {
     if (i < 4) {
-      // Case 1: w[i] = k[i];
-      ranges::copy(key.subspan(4 * i, 4), w[i].begin());
+      ranges::copy(key.subspan(4 * i, 4), words[i].begin());
     } else if (i % 4 == 0) {
-      // Case 2: w[i] = w[i-4] ^ SubWord(RotWord(w[i-1])) ^ Rcon[i/4]
-      std::array<byte, 4> t = w[i - 1];
+      std::array<byte, 4> t = words[i - 1];
       // Rotate left by 1
       ranges::rotate(t, t.begin() + 1);
       // Sub word
@@ -69,11 +70,11 @@ auto aes::KeyExpansion(const std::span<byte, 16> key)
                         [](byte b) { return rijndael::SubByte(b); });
       // Sub rcon
       t[0] ^= RconValues[i / 4];
-      ranges::transform(w[i - 4], t, w[i].begin(), std::bit_xor{});
+      // words[i] = words[i-4] ^ t
+      ranges::transform(words[i - 4], t, words[i].begin(), std::bit_xor{});
     } else {
-      // Case 3: w[i] = w[i-4] ^ w[i-1]
-      ranges::transform(w[i - 4], w[i - 1], w[i].begin(), std::bit_xor{});
+      ranges::transform(words[i - 4], words[i - 1], words[i].begin(), std::bit_xor{});
     }
   }
-  return std::bit_cast<std::array<std::array<byte, 16>, 11>>(w);
+  return std::bit_cast<std::array<std::array<byte, 16>, 11>>(words);
 }
--- src/aes/aes.h
+++ src/aes/aes.h
@@ -15,22 +15,23 @@ namespace aes {
 using byte = uint8_t;
 
 /// @brief Performs the AES SubBytes operation
-/// @param input A span of 16 contiuguous bytes
+/// @param key A span of 16 contiuguous bytes
 /// @param inverse If true, use the inverse S-Box
-auto SubBytes(std::span<byte, 16> input, bool inverse = false) -> void;
+auto SubBytes(std::span<byte, 16> key, bool inverse = false) -> void;
 
 /// @brief Performs the AES ShiftRows operation
-/// @param input A span of 16 contiuguous bytes
+/// @param key A span of 16 contiuguous bytes
 /// @param inverse If true, perform InvShiftRows
-auto ShiftRows(std::span<byte, 16> input, bool inverse = false) -> void;
+auto ShiftRows(std::span<byte, 16> key, bool inverse = false) -> void;
 
 /// @brief Performs the AES MixColumns operation
-/// @param input A span of 16 contiuguous bytes
+/// @param key A span of 16 contiuguous bytes
 /// @param inverse If true, perform InvMixColumns
-auto MixColumns(std::span<byte, 16> input, bool inverse) -> void;
+auto MixColumns(std::span<byte, 16> key, bool inverse) -> void;
 
 /// @brief Performs the AES KeyExpansion operation
 /// @param key An std::array of 16 bytes
+/// @return The initial key and 10 round keys
 auto KeyExpansion(const std::span<byte, 16> key)
     -> std::array<std::array<byte, 16>, 11>;
 }  // namespace aes
